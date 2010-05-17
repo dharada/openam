ba2410e16454fe733c0393b35114a01266e4294a
steve <steve@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2010-05-17 16:27:21 +0000

    Fix for OPENAM-101, backport from TRUNK
    git-svn-id: https://svn.forgerock.org/openam@87 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- branches/opensso_build8_sustaining_branch/opensso/products/federation/library/source/com/sun/identity/saml2/common/SAML2Constants.java
+++ branches/opensso_build8_sustaining_branch/opensso/products/federation/library/source/com/sun/identity/saml2/common/SAML2Constants.java
@@ -26,6 +26,10 @@
  *
  */
 
+/*
+ * Portions Copyrighted [2010] [ForgeRock AS]
+ */
+
 package com.sun.identity.saml2.common;
 
 import com.sun.identity.cot.COTConstants;
@@ -1474,4 +1478,13 @@ public interface SAML2Constants {
      */
     public String INFO_KEY = "infoKey";
 
+    /**
+     * Map key used in SLO request redirect code
+     */
+    public static final String AM_REDIRECT_URL = "AM_REDIRECT_URL";
+
+    /**
+     * Map key used in SLO request redirect code
+     */
+    public static final String OUTPUT_DATA = "OUTPUT_DATA";
 } 
