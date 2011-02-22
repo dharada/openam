0c56a9a87a2e6b0cb126aec88d17320e71d19b37
steve <steve@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2011-02-22 09:37:00 +0000

    Fix for OPENAM-489; backport
    git-svn-id: https://svn.forgerock.org/openam@609 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- branches/openam_snapshot95_sustaining_branch/opensso/products/amserver/source/com/sun/identity/authentication/UI/AuthViewBeanBase.java
+++ branches/openam_snapshot95_sustaining_branch/opensso/products/amserver/source/com/sun/identity/authentication/UI/AuthViewBeanBase.java
@@ -26,7 +26,9 @@
  *
  */
 
-
+/*
+ * Portions Copyrighted 2011 ForgeRock AS
+ */
 
 package com.sun.identity.authentication.UI;
 
@@ -105,7 +107,11 @@ public abstract class AuthViewBeanBase extends ViewBeanBase {
         }
         
         String charset = AuthClientUtils.getCharSet(client_type, accLocale);
-        response.setContentType(content_type+";charset="+charset);
+
+		if (response != null) {
+        	response.setContentType(content_type+";charset="+charset);
+		}
+		
         String jCharset = BrowserEncoding.mapHttp2JavaCharset(charset);
         if (loginDebug.messageEnabled()) {
             loginDebug.message("In setPageEncoding - charset : " + charset);
