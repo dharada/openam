b692e00c2b20d93891863d6e41c27eee883a8c18
peter.major <peter.major@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2011-08-02 11:29:25 +0000

    Fix for OPENAM-738
    git-svn-id: https://svn.forgerock.org/openam@948 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- branches/openam_snapshot95_sustaining_branch/opensso/products/amserver/source/com/iplanet/am/sdk/remote/CachedRemoteServicesImpl.java
+++ branches/openam_snapshot95_sustaining_branch/opensso/products/amserver/source/com/iplanet/am/sdk/remote/CachedRemoteServicesImpl.java
@@ -26,6 +26,9 @@
  *
  */
 
+/**
+ * Portions Copyrighted 2011 ForgeRock AS
+ */
 package com.iplanet.am.sdk.remote;
 
 import java.security.AccessController;
@@ -564,14 +567,10 @@ public class CachedRemoteServicesImpl extends RemoteServicesImpl implements
         // The method below will throw an AMException if the entry does not
         // exist in the directory. If it exists, then create a cache entry for
         // this DN
+        objectType = super.getObjectType(adminToken, entryDN);
         if (cb == null) {
-            objectType = super.getObjectType(adminToken, entryDN);
             cb = new CacheBlock(entryDN, true);
             sdkCache.put(entryDN, cb);
-        } else {
-            objectType = super
-                    .getObjectType(adminToken, entryDN, cb.getAttributes(
-                            MiscUtils.getPrincipalDN(adminToken), false));
         }
         cb.setObjectType(objectType);
         if (objectType == AMObject.ORGANIZATION
