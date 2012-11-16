c45a43bc1f0109daec9b296c516225715fbd56f9
jeff.schenk <jeff.schenk@0f4defcf-c51a-4c67-9f44-6fb5eba73c5d>
2012-11-16 05:20:29 +0000

    AME-211 - Commit for Feature Complete Code Freeze. CR-897 new review for these changes. Final Commit prior to Freeze prior to code reviewed....Fix license in two class source files.
    git-svn-id: https://svn.forgerock.org/openam@3634 0f4defcf-c51a-4c67-9f44-6fb5eba73c5d

--- branches/openam_10.1.0_AME-211/openam/openam-core/src/main/java/com/sun/identity/coretoken/interfaces/AMTokenSAML2Repository.java
+++ branches/openam_10.1.0_AME-211/openam/openam-core/src/main/java/com/sun/identity/coretoken/interfaces/AMTokenSAML2Repository.java
@@ -1,22 +1,32 @@
- /*
+/**
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 ForgeRock US Inc. All Rights Reserved
+ * Copyright (c) 2008 Sun Microsystems, Inc. All Rights Reserved.
  *
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
  *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
+ * You can obtain a copy of the License at
+ * https://opensso.dev.java.net/public/CDDLv1.0.html or
+ * opensso/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
  *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information:
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at opensso/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * "Portions copyright [year] [name of copyright owner]".
- *
+ * $Id: SAML2RepositoryFactory.java,v 1.1 2008/07/22 18:08:20 weisun2 Exp $
+ */
+
+/**
+ * Portions Copyrighted 2010-2012 ForgeRock Inc
  */
 
 package com.sun.identity.coretoken.interfaces;
